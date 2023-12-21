12e1d802472c6af624643c111229457af6600ca8
osamaelshaer <osamabinelshaer@gmail.com>
2023-12-21 10:28:36 +0200

    signals User,Profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from accounts.models import Profile
+from .models import Profile
+
+
+
+
+
+admin.site.register(Profile)
+
+
+'''
 
 # Register your models here.
 # Define an inline admin descriptor for Employee model
@@ -19,3 +28,5 @@ class UserAdmin(BaseUserAdmin):
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+'''
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,9 @@
+import django
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator , MinValueValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -8,3 +11,13 @@ class Profile(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
     phonenumber = models.IntegerField( validators=[MaxValueValidator(100),MinValueValidator(1) ] , null=True , blank=True) 
     addres = models.TextField(max_length=100 , null=True , blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+
+
+@receiver(post_save , sender=User)
+def creat_user_profile (sender , instance , created , **kwargs):
+    if created :
+        Profile.objects.create(user=instance)
